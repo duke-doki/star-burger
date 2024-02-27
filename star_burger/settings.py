5e793e067ba876d2096213f74eaae71b1a60134f
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-02-27 10:39:43 +0300

    setup rollbar

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -40,7 +40,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404',
 ]
+
+ROLLBAR = {
+    'access_token': env.str('ROLLBAR_TOKEN'),
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = 'star_burger.urls'
 
