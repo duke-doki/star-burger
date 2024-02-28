77750dc196f0ff441583831ff0cefaf9725c1b2a
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-02-28 23:35:33 +0300

    Update deploy_star-burger.sh

--- deploy_star-burger.sh
+++ deploy_star-burger.sh
@@ -15,4 +15,20 @@ python3 manage.py collectstatic
 
 systemctl restart star-burger.service
 
+set -a
+[ -f .env ] && . .env
+set +a
+
+curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+{
+  "environment": "production",
+  "revision": "'$(git log -n 1 --pretty=format:"%H")'"
+}
+'
+
 echo "Success!"
